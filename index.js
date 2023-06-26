a384416079cb38b96e1a39424afa02ee791e8fda
sergioincoterm <junior_jr_poa@yahoo.com.br>
2023-06-26 14:44:00 -0300

    atualizado

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 /*
-{ 
-    "logradouro":"Rua teste",
-    "numero":"123",
-    "complemento":" complemento de teste",
-    "cidade":" porto alegre",
-    "estado":"RS",
-    "cep":"90660900"
+Cliente:
+{
+    "id_cli": "2",
+    "codigo": "2A",
+    "name": "CLIENTE TESTE 2"
 }
 
-{
-    "id_cli": 1,
-    "codigo_cli": "556",
-    "nome_cli": "CLIENTE sergio22"
+Endereco:
+{ 
+    "logradouro":"Rua 2 A",
+    "numero":"123",
+    "complemento":" complemento 2 A",
+    "cidade":"SAO PAULO",
+    "estado":"SP",
+    "cep":"9999"
 }
 
 */
 
-const express = require('express');
+
+const express = require("express");
 
 const server = express();
 
@@ -24,89 +27,93 @@ server.use(express.json());
 
 let idcliente = 0;
 let idendereco = 0;
-const clientes  = [];
-const enderecos  = [];
+const clientes = [];
+const enderecos = [];
 
-//Get chave clientes
-server.get('/cliente/:codigo',(req,res) => {
-    const { codigo } = req.params;
-    const cliente = clientes.find(cliente => cliente.codigo_cli == codigo)
-    return res.json(cliente);
-});
-
-//Gett all Cliente
-server.get('/cliente', (req, res) => {
-    return res.json(clientes)
-});
-
-// Post cliente
-server.post('/cliente', (req,res) => {
-    const { name, codigo } = req.body;
-
-    const cliente = clientes.find(cliente => cliente.codigo_cli == codigo);
-
-    if (cliente) {
-        return res.status(400).json({'message':'Cliente já existente'});
-    }
-
-    idcliente++;
-    clientes.push({id_cli:idcliente,
-                 codigo_cli:codigo,
-                 nome_cli:name});
-
-    return res.status(201).send();
-});
-
-//-------------------------------------------------------------------------------------------------------
-
-// Post endereco
-server.post('/cliente/:codigo/endereco', (req,res) => {
-    const { codigo } = req.params;
-
-    const { logradouro, numero, complemento, cidade, estado, cep } = req.body;
-
-    const endereco = enderecos.filter(endereco => endereco.codigo_end == codigo)
-
-    idendereco++;
-    const novoendereco = {id_end:idendereco,
-                            codigo:codigo,
-                            indice_end:endereco.length,
-                            logradouro_end:logradouro,
-                            numero_end:numero,
-                            complemento_end:complemento,
-                            cidade_end:cidade,
-                            estado_end:estado,
-                            cep_end:cep}
-
-    enderecos.push(novoendereco);
-                   
-    return res.status(201).json(novoendereco);
-});
-
-//Get chave endereco
-server.get('/cliente/:codigo/endereco/:indice',(req,res) => {
+//Get chave endereco - /api/v1/cliente/{codigo}/endereco/{indice}
+server.get("/cliente/:codigo/endereco/:indice", (req, res) => {
     const { indice, codigo } = req.params;
-    const endereco = enderecos.find(endereco => {
-        if (endereco.indice_end == indice && endereco.codigo == codigo){
-            return endereco
-        }
-    })
-    console.log(endereco);
+    const endereco = enderecos.find((endereco) => {
+      if (endereco.indice_end == indice && endereco.codigo == codigo) {
+        return endereco;
+      }
+    });
     return res.json(endereco);
-});
-
-//Get chave endereco
-server.get('/cliente/:codigo/endereco',(req,res) => {
+  });
+  
+  //Get chave endereco - /api/v1/cliente/{codigo}/endereco/
+  server.get("/cliente/:codigo/endereco", (req, res) => {
     const { codigo } = req.params;
-    const enderecos = enderecos.filter(endereco => endereco.codigo == codigo )
+    const enderecosResult = enderecos.filter(
+      (endereco) => endereco.codigo == codigo
+    );
+    return res.json(enderecosResult);
+  });
+  
+  //Get chave cidade - /api/v1/cliente/endereco?cidade={cidade}
+server.get("/cliente/endereco",(req,res) => {
+    const { cidade } = req.query;
+
+    const enderecosResult = enderecos.filter(endereco => endereco.cidade_end.startsWith(cidade.toString()));
+  
     return res.json(enderecos);
 });
 
-//Get chave cidade
-server.get('/cliente/endereco',(req,res) => {
-    const { cidade } = req.query
-    const enderecos = enderecos.filter(endereco => endereco.cidade_end == cidade )
-    return res.json(enderecos);
+
+//Get chave clientes - /api/v1/cliente/{codigo}
+server.get("/cliente/:codigo", (req, res) => {
+  const { codigo } = req.params;
+  const cliente = clientes.find((cliente) => cliente.codigo_cli == codigo);
+  return res.json(cliente);
+});
+
+//Get all Cliente - /api/v1/cliente/
+server.get("/cliente", (req, res) => {
+  return res.json(clientes);
+});
+
+//-----------------------------------------------------------------------------------------------------
+
+// Post cliente - POST /api/v1/cliente
+server.post("/cliente", (req, res) => {
+  const { name, codigo } = req.body;
+
+  const cliente = clientes.find((cliente) => cliente.codigo_cli == codigo);
+
+  if (cliente) {
+    return res.status(400).json({ message: "Cliente já existente" });
+  }
+
+  idcliente++;
+  clientes.push({ id_cli: idcliente, codigo_cli: codigo, nome_cli: name });
+
+  return res.status(201).send();
+});
+
+// Post endereco - /api/v1/cliente/{codigo}/endereco
+server.post("/cliente/:codigo/endereco", (req, res) => {
+  const { codigo } = req.params;
+
+  const { logradouro, numero, complemento, cidade, estado, cep } = req.body;
+
+  const endereco = enderecos.filter((endereco) => endereco.codigo == codigo);
+
+  idendereco++;
+  const novoendereco = {
+    id_end: idendereco,
+    codigo: codigo,
+    indice_end: endereco.length + 1,
+    logradouro_end: logradouro,
+    numero_end: numero,
+    complemento_end: complemento,
+    cidade_end: cidade,
+    estado_end: estado,
+    cep_end: cep,
+  };
+
+  enderecos.push(novoendereco);
+
+  return res.status(201).json(novoendereco);
 });
 
 server.listen(3000);
